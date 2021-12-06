8eeb227894dfc189455d19f025e64ff6318ad452
notgurev <notgurev@gmail.com>
2021-12-06 10:30:55 +0300

    readable vfsmount flags

--- user/main.c
+++ user/main.c
@@ -43,6 +43,48 @@ struct message_to_kernel {
     int socketfd;
 };
 
+#define MNT_NOSUID    0x01
+#define MNT_NODEV    0x02
+#define MNT_NOEXEC    0x04
+#define MNT_NOATIME    0x08
+#define MNT_NODIRATIME    0x10
+#define MNT_RELATIME    0x20
+#define MNT_READONLY    0x40
+#define MNT_NOSYMFOLLOW    0x80
+#define MNT_SHRINKABLE  0x100
+#define MNT_WRITE_HOLD  0x200
+#define MNT_SHARED  0x1000 
+#define MNT_UNBINDABLE  0x2000  
+
+#define flagdef(name, value) flags[value] = #name
+
+char *flags[MNT_UNBINDABLE + 1];
+
+void init_flags() {
+    flagdef(MNT_NOSUID, 0x01);
+    flagdef(MNT_NODEV, 0x02);
+    flagdef(MNT_NOEXEC, 0x04);
+    flagdef(MNT_NOATIME, 0x08);
+    flagdef(MNT_NODIRATIME, 0x10);
+    flagdef(MNT_RELATIME, 0x20);
+    flagdef(MNT_READONLY, 0x40);
+    flagdef(MNT_NOSYMFOLLOW, 0x80);
+    flagdef(MNT_SHRINKABLE,  0x100);
+    flagdef(MNT_WRITE_HOLD,  0x200);
+    flagdef(MNT_SHARED,  0x1000);
+    flagdef(MNT_UNBINDABLE , 0x2000);  
+}
+
+void print_flags(int number) {
+    printf("Flags: ");
+    for (int mask = 1; mask <= MNT_UNBINDABLE; mask = mask << 1) {
+        if ((number & mask) != 0) {
+            printf("%s ", flags[mask]);
+        }
+    }
+    puts("");
+}
+
 int main(int argc, char *argv[]) {
     if (argc < 1) {
         puts("wrong number of arguments, expected 1");
@@ -83,8 +125,11 @@ int main(int argc, char *argv[]) {
     struct vfsmount_cut vc = msg.vfs_cut;
     struct socket_cut sc = msg.socket_cut;
 
+    init_flags();
+
     puts(" --- vfsmount --- ");
-    printf("Flags: %#0x\n", vc.mnt_flags);
+    printf("Flags (hex): %0x\n", vc.mnt_flags);
+    print_flags(vc.mnt_flags);
     printf("Block size in bits: %u\n", vc.s_blocksize_bits);
     printf("Block size in bytes: %lu\n", vc.s_blocksize);
     printf("Reference count: %d\n", vc.s_count);
